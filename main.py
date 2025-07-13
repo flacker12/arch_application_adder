03cf7ec6ec655bd5a4abf212c2a9194d3cfccb21
flacker12 <leonid300410@gmail.com>
2025-07-13 15:45:31 +0300

    1.1

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import sys
 
 path = os.getcwd()
 file = sys.argv[1]
+user = os.getlogin()
 
 name=input("Enter the name of application: ")
 
 
-os.system(f"touch /home/daffuk/.local/share/applications/{file}.desktop")
-os.system(f'echo "[Desktop Entry]\nExec={path}/{file}\nName={name}\nType=Application" > /home/daffuk/.local/share/applications/{file}.desktop')
+os.system(f"touch /home/{user}/.local/share/applications/{file}.desktop")
+os.system(f'echo "[Desktop Entry]\nExec={path}/{file}\nName={name}\nType=Application" > /home/{user}/.local/share/applications/{file}.desktop')
 print("done")
